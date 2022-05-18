b40f6cc46d672f992b81102f3b4780317d60ca54
Dulakshi RB <dulakshirb@gmail.com>
2022-05-19 01:13:44 +0530

    Navigation Drawer

--- HarithaFarmer/app/src/main/java/com/haritha/haritha_farmer/MainActivity.java
+++ HarithaFarmer/app/src/main/java/com/haritha/haritha_farmer/MainActivity.java
@@ -1,14 +1,84 @@
 package com.haritha.haritha_farmer;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle toggle;
+    Toolbar toolbar;
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        drawerLayout = findViewById(R.id.drawer);
+        toolbar = findViewById(R.id.toolBar);
+        setSupportActionBar(toolbar);
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                Fragment fragment = null;
+                switch (id) {
+                    case R.id.nav_task_and_schedule:
+                        fragment = new TaskAndScheduleFragment();
+                        loadFragment(fragment);
+                        break;
+                    case R.id.nav_crops:
+                        fragment = new CropsFragment();
+                        loadFragment(fragment);
+                        break;
+                    case R.id.nav_resources:
+                        break;
+                    case R.id.nav_fertilizing:
+                        break;
+                    case R.id.nav_seeding:
+                        break;
+                    case R.id.nav_predictive_analytics:
+                        break;
+                    case R.id.nav_security:
+                        break;
+                    case R.id.nav_watering_and_soilHealth:
+                        break;
+                    case R.id.nav_reports:
+                        break;
+                    case R.id.nav_settings:
+                        break;
+                    case R.id.nav_support:
+                        break;
+                    case R.id.nav_logout:
+                        break;
+                    default:
+                        return true;
+                }
+                return true;
+            }
+        });
+    }
+
+    private void loadFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame, fragment).commit();
+        drawerLayout.closeDrawer(GravityCompat.START);
+        fragmentTransaction.addToBackStack(null);
     }
 }
