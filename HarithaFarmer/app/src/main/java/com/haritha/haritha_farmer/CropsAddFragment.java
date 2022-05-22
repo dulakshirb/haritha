9b405b55f8a0f8cc8116b2d37065e56b94341e5e
Dulakshi RB <dulakshirb@gmail.com>
2022-05-22 10:57:05 +0530

    added date picker

--- HarithaFarmer/app/src/main/java/com/haritha/haritha_farmer/CropsAddFragment.java
+++ HarithaFarmer/app/src/main/java/com/haritha/haritha_farmer/CropsAddFragment.java
@@ -1,21 +1,59 @@
 package com.haritha.haritha_farmer;
 
+
+import static android.content.ContentValues.TAG;
+
+import android.app.DatePickerDialog;
+import android.nfc.Tag;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
+
+import java.util.Calendar;
 
 public class CropsAddFragment extends Fragment {
+
+    private AppCompatButton plantedDate;
+    private DatePickerDialog.OnDateSetListener dateSetListener;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_crops_add, container, false);
+
+        plantedDate = (AppCompatButton) view.findViewById(R.id.calendar_plantedDate);
+        plantedDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog datePickerDialog = new DatePickerDialog(getContext(), android.R.style.Theme_DeviceDefault_Dialog, dateSetListener, year, month, day);
+                datePickerDialog.show();
+            }
+        });
+
+        dateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                month = month + 1;
+                Log.d(TAG, "onDateSer: dd/mm/yyyy: " + day + "/" + month + "/" + year);
+                String date = day + "/" + month + "/" + year;
+                plantedDate.setText(date);
+            }
+        };
+
         return view;
     }
 }
