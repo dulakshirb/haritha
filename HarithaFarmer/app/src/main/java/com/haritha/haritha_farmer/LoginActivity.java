8f78c0fd4b67ba0192fd9e463cb7f62ffa9d0fa3
Dulakshi RB <dulakshirb@gmail.com>
2022-05-25 13:11:10 +0530

    Login & signup UI update

--- HarithaFarmer/app/src/main/java/com/haritha/haritha_farmer/LoginActivity.java
+++ HarithaFarmer/app/src/main/java/com/haritha/haritha_farmer/LoginActivity.java
@@ -4,17 +4,43 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseUser;
+
+import org.w3c.dom.Text;
 
 public class LoginActivity extends AppCompatActivity {
 
     private FirebaseUser currentUser;
+    private EditText loginEmail, loginPassword;
+    private Button btnLogin;
+    private TextView forgetPasswordLink, newUserRegisterLink;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        initializeField();
+
+        newUserRegisterLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendUsertoRegisterActivity();
+            }
+        });
+    }
+
+    private void initializeField() {
+        loginEmail = (EditText) findViewById(R.id.login_email);
+        loginPassword = (EditText) findViewById(R.id.login_password);
+        forgetPasswordLink = (TextView) findViewById(R.id.forget_password_link);
+        newUserRegisterLink = (TextView) findViewById(R.id.new_user_register_link);
+        btnLogin = (Button) findViewById(R.id.btn_login);
     }
 
     @Override
@@ -29,5 +55,10 @@ public class LoginActivity extends AppCompatActivity {
     private void sendUsertoMainActivity() {
         Intent loginIntent = new Intent(LoginActivity.this, MainActivity.class);
         startActivity(loginIntent);
+    }
+
+    private void sendUsertoRegisterActivity() {
+        Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class);
+        startActivity(registerIntent);
     }
 }
--- HarithaFarmer/app/src/main/java/com/haritha/haritha_farmer/RegisterActivity.java
+++ HarithaFarmer/app/src/main/java/com/haritha/haritha_farmer/RegisterActivity.java
@@ -2,13 +2,44 @@ package com.haritha.haritha_farmer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private EditText userEmail, userPassword;
+    private Button btnRegister;
+    private TextView alreadyHaveanAccountLink;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        initializeField();
+
+        alreadyHaveanAccountLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendUsertoLoginActivity();
+            }
+        });
     }
+
+    private void initializeField() {
+        userEmail = (EditText) findViewById(R.id.register_email);
+        userPassword = (EditText) findViewById(R.id.register_password);
+        btnRegister = (Button) findViewById(R.id.btn_register);
+        alreadyHaveanAccountLink = (TextView) findViewById(R.id.already_have_an_account_link);
+    }
+
+    private void sendUsertoLoginActivity() {
+        Intent loginIntent = new Intent(RegisterActivity.this, LoginActivity.class);
+        startActivity(loginIntent);
+    }
+
 }
