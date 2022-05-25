dac680b21a53e25021883ea1bfa1f32f47310990
iamhirusha <hirushaperera98@gmail.com>
2022-05-26 02:43:06 +0530

    reports

--- HarithaFarmer/app/src/main/java/com/haritha/haritha_farmer/ResourcesFragment.java
+++ HarithaFarmer/app/src/main/java/com/haritha/haritha_farmer/ResourcesFragment.java
@@ -1,59 +1,76 @@
 package com.haritha.haritha_farmer;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 public class ResourcesFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public ResourcesFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ResourcesFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ResourcesFragment newInstance(String param1, String param2) {
-        ResourcesFragment fragment = new ResourcesFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
+    private View view;
+    private ImageButton img_inventory,img_fertilizer,img_tools,img_vehicle,img_seeds;
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        view = inflater.inflate(R.layout.fragment_resources, container, false);
+
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+            img_fertilizer = view.findViewById(R.id.img_fertilizer);
+            img_inventory = view.findViewById(R.id.img_inventory);
+            img_seeds = view.findViewById(R.id.img_seeds);
+            img_tools = view.findViewById(R.id.img_tools);
+            img_vehicle = view.findViewById(R.id.img_vehicle);
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_resources, container, false);
+            img_vehicle.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    AppCompatActivity activity = (AppCompatActivity) view.getContext();
+                    Fragment vehicleFragment = new Vehicles();
+                    activity.getSupportFragmentManager().beginTransaction().replace(R.id.frame, vehicleFragment).addToBackStack(null).commit();
+                }
+            });
+            img_tools.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    AppCompatActivity activity = (AppCompatActivity) view.getContext();
+                    Fragment toolsFragment = new ToolsFragment();
+                    activity.getSupportFragmentManager().beginTransaction().replace(R.id.frame, toolsFragment).addToBackStack(null).commit();
+                }
+            });
+            img_seeds.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    AppCompatActivity activity = (AppCompatActivity) view.getContext();
+                    Fragment seedsFragment = new SeedsFragment();
+                    activity.getSupportFragmentManager().beginTransaction().replace(R.id.frame, seedsFragment).addToBackStack(null).commit();
+                }
+            });
+            img_inventory.setOnClickListener(new View.OnClickListener() {
+               @Override
+               public void onClick(View v) {
+                   AppCompatActivity activity = (AppCompatActivity) view.getContext();
+                   Fragment inventoryFragment = new InventoryFertilizerFragment();
+                   activity.getSupportFragmentManager().beginTransaction().replace(R.id.frame, inventoryFragment).addToBackStack(null).commit();
+               }
+           });
+            img_fertilizer.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    AppCompatActivity activity = (AppCompatActivity) view.getContext();
+                    Fragment fertilizerFragment = new FertilizerFragment();
+                    activity.getSupportFragmentManager().beginTransaction().replace(R.id.frame, fertilizerFragment).addToBackStack(null).commit();
+
+                }
+            });
+
+
+
+
+return view;
     }
 }
