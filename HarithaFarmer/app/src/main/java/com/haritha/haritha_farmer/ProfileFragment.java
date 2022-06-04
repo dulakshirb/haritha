6af3ab345cbfa94e4e23762dbda8f66b6fbedfb3
Dulakshi RB <dulakshirb@gmail.com>
2022-06-04 10:24:14 +0530

    Update Profile Fragment

--- HarithaFarmer/app/src/main/java/com/haritha/haritha_farmer/ProfileFragment.java
+++ HarithaFarmer/app/src/main/java/com/haritha/haritha_farmer/ProfileFragment.java
@@ -1,42 +1,22 @@
 package com.haritha.haritha_farmer;
 
-import static android.app.Activity.RESULT_FIRST_USER;
-import static android.app.Activity.RESULT_OK;
-
-import android.Manifest;
-import android.app.AlertDialog;
 import android.app.ProgressDialog;
-import android.content.ContentValues;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
-
-import androidx.activity.result.ActivityResult;
-import androidx.activity.result.ActivityResultCallback;
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.widget.AppCompatImageView;
-import androidx.core.content.ContextCompat;
-import androidx.fragment.app.Fragment;
-
-import android.provider.MediaStore;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnSuccessListener;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -46,15 +26,10 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
-import com.google.firebase.storage.UploadTask;
-import com.haritha.haritha_farmer.MainActivity;
-import com.haritha.haritha_farmer.R;
 import com.squareup.picasso.Picasso;
 
-import java.io.IOException;
 import java.util.HashMap;
-
-import javax.xml.transform.Result;
+import java.util.Objects;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -67,7 +42,6 @@ public class ProfileFragment extends Fragment {
     private ProgressDialog loadingBar;
 
     private String currentUserId;
-    private FirebaseAuth mAuth;
     private DatabaseReference rootRef;
     private StorageReference userProfileImagesRef;
     String downloadUrl;
@@ -82,70 +56,47 @@ public class ProfileFragment extends Fragment {
                              Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_profile, container, false);
 
-        mAuth = FirebaseAuth.getInstance();
-        currentUserId = mAuth.getCurrentUser().getUid();
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        currentUserId = Objects.requireNonNull(mAuth.getCurrentUser()).getUid();
         rootRef = FirebaseDatabase.getInstance().getReference();
         userProfileImagesRef = FirebaseStorage.getInstance().getReference().child("Profile Images");
 
-        activityResultLauncher = registerForActivityResult(new ActivityResultContracts.GetContent(), new ActivityResultCallback<Uri>() {
-            @Override
-            public void onActivityResult(Uri result) {
-                profileImage.setImageURI(result);
+        activityResultLauncher = registerForActivityResult(new ActivityResultContracts.GetContent(), result -> {
+            profileImage.setImageURI(result);
 
-                loadingBar.setTitle("Set Profile Image");
-                loadingBar.setMessage("Please wait, your profile image is uploading...");
-                loadingBar.setCanceledOnTouchOutside(true);
-                loadingBar.show();
+            loadingBar.setTitle("Set Profile Image");
+            loadingBar.setMessage("Please wait, your profile image is uploading...");
+            loadingBar.setCanceledOnTouchOutside(true);
+            loadingBar.show();
 
-                StorageReference filePath = userProfileImagesRef.child(currentUserId + ".jpg");
-                filePath.putFile(result).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                    @Override
-                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                        Toast.makeText(getActivity(), "Profile Image Uploaded Successfully...", Toast.LENGTH_SHORT).show();
-                        final Task<Uri> firebaseUri = taskSnapshot.getStorage().getDownloadUrl();
-                        firebaseUri.addOnSuccessListener(new OnSuccessListener<Uri>() {
-                            @Override
-                            public void onSuccess(Uri uri) {
-                                downloadUrl = uri.toString();
-                                rootRef.child("Farmer").child("Users").child(currentUserId).child("image").setValue(downloadUrl)
-                                        .addOnCompleteListener(new OnCompleteListener<Void>() {
-                                            @Override
-                                            public void onComplete(@NonNull Task<Void> task) {
-                                                if (task.isSuccessful()) {
-                                                    Toast.makeText(getActivity(), "Image save in database successfully.", Toast.LENGTH_SHORT).show();
-                                                    loadingBar.dismiss();
-                                                    retrieveProfileInfo();
-                                                } else {
-                                                    String message = task.getException().toString();
-                                                    Toast.makeText(getActivity(), "Error: " + message, Toast.LENGTH_SHORT).show();
-                                                    loadingBar.dismiss();
-                                                }
-                                            }
-                                        });
-                            }
-                        });
-                    }
+            StorageReference filePath = userProfileImagesRef.child(currentUserId + ".jpg");
+            filePath.putFile(result).addOnSuccessListener(taskSnapshot -> {
+                Toast.makeText(getActivity(), "Profile Image Uploaded Successfully...", Toast.LENGTH_SHORT).show();
+                final Task<Uri> firebaseUri = taskSnapshot.getStorage().getDownloadUrl();
+                firebaseUri.addOnSuccessListener(uri -> {
+                    downloadUrl = uri.toString();
+                    rootRef.child("Farmer").child("Users").child(currentUserId).child("image").setValue(downloadUrl)
+                            .addOnCompleteListener(task -> {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(getActivity(), "Image save in database successfully.", Toast.LENGTH_SHORT).show();
+                                    loadingBar.dismiss();
+                                    retrieveProfileInfo();
+                                } else {
+                                    String message = Objects.requireNonNull(task.getException()).toString();
+                                    Toast.makeText(getActivity(), "Error: " + message, Toast.LENGTH_SHORT).show();
+                                    loadingBar.dismiss();
+                                }
+                            });
                 });
-            }
+            });
         });
 
         initializeFields();
         retrieveProfileInfo();
 
-        profileImage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                activityResultLauncher.launch("image/*");
-            }
-        });
+        profileImage.setOnClickListener(view -> activityResultLauncher.launch("image/*"));
 
-        btnUpdateProfile.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                updateProfile();
-            }
-        });
+        btnUpdateProfile.setOnClickListener(view -> updateProfile());
 
         return view;
     }
@@ -171,7 +122,6 @@ public class ProfileFragment extends Fragment {
         if (TextUtils.isEmpty(setUserName)) {
             userName.setError("Please enter your name");
             userName.requestFocus();
-            return;
         } else {
             HashMap<String, String> profileMap = new HashMap<>();
             profileMap.put("userID", currentUserId);
@@ -179,17 +129,14 @@ public class ProfileFragment extends Fragment {
             profileMap.put("userName", setUserName);
             profileMap.put("image", downloadUrl);
             rootRef.child("Farmer").child("Users").child(currentUserId).setValue(profileMap)
-                    .addOnCompleteListener(new OnCompleteListener<Void>() {
-                        @Override
-                        public void onComplete(@NonNull Task<Void> task) {
-                            if (task.isSuccessful()) {
-                                sendUsertoMainActivity();
-                                Toast.makeText(getActivity(), "Profile updated successfully...", Toast.LENGTH_SHORT).show();
-                            } else {
-                                String message = task.getException().toString();
-                                System.out.println(message);
-                                Toast.makeText(getActivity(), "Error: " + message, Toast.LENGTH_SHORT).show();
-                            }
+                    .addOnCompleteListener(task -> {
+                        if (task.isSuccessful()) {
+                            sendUserToMainActivity();
+                            Toast.makeText(getActivity(), "Profile updated successfully...", Toast.LENGTH_SHORT).show();
+                        } else {
+                            String message = Objects.requireNonNull(task.getException()).toString();
+                            System.out.println(message);
+                            Toast.makeText(getActivity(), "Error: " + message, Toast.LENGTH_SHORT).show();
                         }
                     });
         }
@@ -201,21 +148,21 @@ public class ProfileFragment extends Fragment {
                     @Override
                     public void onDataChange(@NonNull DataSnapshot snapshot) {
                         if ((snapshot.exists()) && (snapshot.hasChild("farmName") && snapshot.hasChild("image"))) {
-                            String retFarmName = snapshot.child("farmName").getValue().toString();
-                            String retUserName = snapshot.child("userName").getValue().toString();
-                            String retProfileImage = snapshot.child("image").getValue().toString();
+                            String retFarmName = Objects.requireNonNull(snapshot.child("farmName").getValue()).toString();
+                            String retUserName = Objects.requireNonNull(snapshot.child("userName").getValue()).toString();
+                            String retProfileImage = Objects.requireNonNull(snapshot.child("image").getValue()).toString();
                             farmName.setText(retFarmName);
                             userName.setText(retUserName);
                             System.out.println(retProfileImage);
                             Picasso.get().load(retProfileImage).into(profileImage);
                             //Glide.with(ProfileFragment.this).load(retProfileImage).into(profileImage);
                         } else if ((snapshot.exists()) && (snapshot.hasChild("farmName"))) {
-                            String retFarmName = snapshot.child("farmName").getValue().toString();
-                            String retUserName = snapshot.child("userName").getValue().toString();
+                            String retFarmName = Objects.requireNonNull(snapshot.child("farmName").getValue()).toString();
+                            String retUserName = Objects.requireNonNull(snapshot.child("userName").getValue()).toString();
                             farmName.setText(retFarmName);
                             userName.setText(retUserName);
                         } else if ((snapshot.exists()) && snapshot.hasChild("image")){
-                            String retProfileImage = snapshot.child("image").getValue().toString();
+                            String retProfileImage = Objects.requireNonNull(snapshot.child("image").getValue()).toString();
                             Picasso.get().load(retProfileImage).into(profileImage);
                         } else{
                             Toast.makeText(getActivity(), "Please set & update your profile information.", Toast.LENGTH_SHORT).show();
@@ -228,7 +175,7 @@ public class ProfileFragment extends Fragment {
                 });
     }
 
-    private void sendUsertoMainActivity() {
+    private void sendUserToMainActivity() {
         Intent mainIntent = new Intent(getActivity(), MainActivity.class);
         startActivity(mainIntent);
     }
