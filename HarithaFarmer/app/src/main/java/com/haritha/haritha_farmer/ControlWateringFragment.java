8ea5f31531db388f69a3fcfe9ce282aabe219f8c
iamhirusha <hirushaperera98@gmail.com>
2022-06-21 04:30:38 +0530

    watering

--- HarithaFarmer/app/src/main/java/com/haritha/haritha_farmer/ControlWateringFragment.java
+++ HarithaFarmer/app/src/main/java/com/haritha/haritha_farmer/ControlWateringFragment.java
@@ -8,57 +8,76 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ControlWateringFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import android.graphics.Color;
+import android.support.annotation.NonNull;
+//import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+
 public class ControlWateringFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private View view;
+    private TextView text_soilmoisture;
+    private String soilmoisture;
+    private FirebaseAuth mAuth;
+    private FirebaseUser currentUser;
+    private DatabaseReference dref;
 
     public ControlWateringFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ControlSecurityFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ControlWateringFragment newInstance(String param1, String param2) {
-        ControlWateringFragment fragment = new ControlWateringFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_control_watering, container, false);
+
+        view = inflater.inflate(R.layout.fragment_control_watering, container, false);
+        text_soilmoisture = view.findViewById(R.id.txt_soil_health);
+
+        mAuth = FirebaseAuth.getInstance();
+        currentUser = mAuth.getCurrentUser();
+        String userId = currentUser.getUid();
+        dref = FirebaseDatabase.getInstance().getReference("Farmer").child("SoilHealth").child(userId);
+
+        dref.child("health").addValueEventListener(new ValueEventListener(){
+      //  dref = FirebaseDatabase.getInstance().getReference();
+        //dref.addValueEventListener(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                soilmoisture = dataSnapshot.child("health").getValue().toString();
+                int soilData = Integer.parseInt(soilmoisture);
+
+
+                if (soilData == 0) {
+
+                    text_soilmoisture.setText("No watering required.");
+                  //  text_soilmoisture.setTextColor(col2);
+
+                }
+                else {
+                    text_soilmoisture.setText("Watering required!");
+                   // text_soilmoisture.setTextColor(col1);
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+        return view;
     }
 }
